79f4f5279d8b7abdca63bc251e501c2f4bd6ec8f
latifsk <latifsk39@gmail.com>
2022-08-30 10:56:50 +0000

    "updt"

--- screens/sti.js
+++ screens/sti.js
@@ -1,11 +1,24 @@
+import { Ionicons } from "@expo/vector-icons";
 import React, { useState } from "react";
-import { View, Text, ScrollView, FlatList } from "react-native";
+import { View, Text, ScrollView, FlatList, TouchableOpacity, Modal } from "react-native";
 
 export default function STI(){
 
+// state for opening and closing modal
+    const [modalOpen, setModal]= useState(false)
+
 // state for keeping data on STI
     const [stiInfo, setInfo]= useState([
-        {key:1, STIname:"Human papillomavirus (HPV)", STIsymptopns:"", STIcause:"", STIremedy:"", STIadvice:""},
+        {key:1, 
+        STIname:"Human papillomavirus (HPV)", 
+        STIsymptopns:["Symptoms of HPV may appear years after the initial infection with a variety of symptoms including:",
+        "Warts of many kind (HPV-6, HPV-11) – genital warts, oropharyngeal warts, flat warts, plantar warts etc. (HPV-16, HPV-18 don\’t cause warts but can lead to cancer",
+        "Trouble swallowing", "Many infected persons never have symptoms", "Unexplained weight loss", "Unusual growths", "Lumps", "Constant sores", "Constant lesions", "Coughing up blood"],
+        STIcause:"", 
+        STIremedy:"", 
+        STIadvice:""},
+        
+        
         {key:2, STIname:"Syphillis", STIsymptopns:"", STIcause:"", STIremedy:"", STIadvice:""},
         {key:3, STIname:"Gono", STIsymptopns:"", STIcause:"", STIremedy:"", STIadvice:""},
         {key:4, STIname:"HIV", STIsymptopns:"", STIcause:"", STIremedy:"", STIadvice:""},
@@ -21,16 +34,16 @@ export default function STI(){
 
 return(
 
-    <View style={{backgroundColor:"pink", height:"100%"}}>
-        {/* <ScrollView> */}
+    <View style={{backgroundColor:"rgba(255,110,180,0.3)", height:"100%"}}>
+
         <FlatList
         data={stiInfo}
         renderItem = {({item})=>{
 
                 return(
 
-                    <View key={item.key} style={{borderWidth:0.3, borderRadius:5, backgroundColor:"white", marginHorizontal:"3%", marginTop:"5%",
-                            flexDirection:"row"
+                    <TouchableOpacity onPress={()=>{setModal === false ? true : false}} activeOpacity={0.8} key={item.key} 
+                    style={{borderWidth:0.3, borderRadius:5, backgroundColor:"white", marginHorizontal:"3%", marginTop:"5%",flexDirection:"row"
                     }}>
                         <View style={{padding:"4%", backgroundColor:"rgba(150,150,150,0.1)", width:"17%",
                             justifyContent:"center", alignItems:"center"
@@ -47,11 +60,28 @@ return(
                         <View style={{padding:"5%", justifyContent:"center", alignItems:"center" }}>
                             <Text style={{fontSize:16}}>{item.STIname}</Text>
                         </View>    
-                    </View>
+                    </TouchableOpacity>
                 )
             }}
             />
-        {/* </ScrollView> */}
+
+
+            <Modal visible={modalOpen} >
+                <View>
+                    <TouchableOpacity onPress={()=>{setModal === true ? false : true}}>
+                        <Ionicons name="close" size={20} color={"red"} />
+                    </TouchableOpacity>
+                    <View>
+                        <Text>heading here</Text>
+                    </View>
+
+                    <View>
+
+                    </View>
+                </View>
+            </Modal>
+
+
     </View>
 )
 }
